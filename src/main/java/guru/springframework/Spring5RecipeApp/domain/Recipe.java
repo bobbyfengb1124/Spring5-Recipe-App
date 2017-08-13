805ddd97292bb3e898aca90a8f882e7e38fb8a09
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-08-13 11:47:19 +0800

    added Recipe and Ingredient OneToMany JPA modeling

--- src/main/java/guru/springframework/Spring5RecipeApp/domain/Recipe.java
+++ src/main/java/guru/springframework/Spring5RecipeApp/domain/Recipe.java
@@ -4,12 +4,15 @@
  */
 package guru.springframework.Spring5RecipeApp.domain;
 
+import java.util.Set;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 /**
@@ -31,6 +34,9 @@ public class Recipe {
 	private String directions;
 	// TODO add
 	// private Difficulty difficulty;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	private Set<Ingredient> ingredients;
 
 	@Lob
 	private Byte[] image;
@@ -116,5 +122,13 @@ public class Recipe {
 
 	public void setNotes(Notes notes) {
 		this.notes = notes;
+	}
+
+	public Set<Ingredient> getIngredients() {
+		return ingredients;
+	}
+
+	public void setIngredients(Set<Ingredient> ingredients) {
+		this.ingredients = ingredients;
 	}
 }
