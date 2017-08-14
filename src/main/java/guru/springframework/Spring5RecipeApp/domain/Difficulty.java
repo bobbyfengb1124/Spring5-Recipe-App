ca6eb8f070b24690949f78d3567be62627ea0ac4
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-08-14 11:45:12 +0800

    added enumerated type Difficulty

---
+++ src/main/java/guru/springframework/Spring5RecipeApp/domain/Difficulty.java
@@ -0,0 +1,13 @@
+/**
+ * @Author Feng Bo
+ * @Date 14 Aug 2017 11:37:22 am
+ */
+package guru.springframework.Spring5RecipeApp.domain;
+
+/**
+ * 
+ */
+public enum Difficulty {
+
+	EASY, MODERATE, HARD
+}
