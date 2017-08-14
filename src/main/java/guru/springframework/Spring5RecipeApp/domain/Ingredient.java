0cf715a1edc8004398a03358471e764f83ef52fc
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-08-14 11:33:42 +0800

    added OneToOne UnitOfMeasure relationship

--- src/main/java/guru/springframework/Spring5RecipeApp/domain/Ingredient.java
+++ src/main/java/guru/springframework/Spring5RecipeApp/domain/Ingredient.java
@@ -7,10 +7,12 @@ package guru.springframework.Spring5RecipeApp.domain;
 import java.math.BigDecimal;
 
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 
 /**
  * 
@@ -28,7 +30,8 @@ public class Ingredient {
 	@ManyToOne
 	private Recipe recipe;
 
-	// private UnitOfMeasure uom;
+	@OneToOne(fetch = FetchType.EAGER)
+	private UnitOfMeasure uom;
 
 	public Long getId() {
 		return id;
@@ -60,6 +63,14 @@ public class Ingredient {
 
 	public void setRecipe(Recipe recipe) {
 		this.recipe = recipe;
+	}
+
+	public UnitOfMeasure getUom() {
+		return uom;
+	}
+
+	public void setUom(UnitOfMeasure uom) {
+		this.uom = uom;
 	}
 
 }
