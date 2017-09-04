bb087986de59a5cf759e7deba3201469e1732131
bobbyfeng <bobbyfeng@Bobby01-NB.cse.com.sg>
2017-09-04 14:03:01 +0800

    added Exception Handler for redirect a error page to users

--- src/main/java/guru/springframework/Spring5RecipeApp/controllers/RecipeController.java
+++ src/main/java/guru/springframework/Spring5RecipeApp/controllers/RecipeController.java
@@ -4,15 +4,20 @@
  */
 package guru.springframework.Spring5RecipeApp.controllers;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
 
 import guru.springframework.Spring5RecipeApp.commands.RecipeCommand;
+import guru.springframework.Spring5RecipeApp.exceptions.NotFoundException;
 import guru.springframework.Spring5RecipeApp.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -73,5 +78,18 @@ public class RecipeController {
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
 
 		return "redirect:/recipe/" + savedCommand.getId() + "/show";
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public ModelAndView handleNotFound() {
+
+		log.error("Handling not found exception");
+
+		ModelAndView modelAndView = new ModelAndView();
+
+		modelAndView.setViewName("404error");
+		return modelAndView;
+
 	}
 }
--- src/test/java/guru/springframework/Spring5RecipeApp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/Spring5RecipeApp/controllers/RecipeControllerTest.java
@@ -134,7 +134,7 @@ public class RecipeControllerTest {
 
 		when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
-		mockMvc.perform(get("/recipe/1/show")).andExpect(status().isNotFound());
+		mockMvc.perform(get("/recipe/1/show")).andExpect(status().isNotFound()).andExpect(view().name("404error"));
 	}
 
 }
